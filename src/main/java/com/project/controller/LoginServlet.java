0a5901fbd8141e6196754755a494fb827f86fcf5
Maneesh <manishgangwar373@gmail.com>
2025-11-28 00:45:23 +0530

    Implement LoginServlet for user authentication

---
+++ src/main/java/com/project/controller/LoginServlet.java
@@ -0,0 +1,29 @@
+package com.project.controller;
+
+import com.project.dao.UserDAO;
+import com.project.model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UserDAO userDAO = new UserDAO();
+        User user = userDAO.login(email, password);
+
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            response.sendRedirect("login.jsp?error=1");
+        }
+    }
+}
